91b4bff2a48d026f6c3cebba73d4f6540c3b4e69
Maxime Montfort <maximemontfort.pro@gmail.com>
2024-11-15 20:41:13 +0100

    new stuff

--- LABS-INTERNAL/src/components/mod.rs
+++ LABS-INTERNAL/src/components/mod.rs
@@ -13,12 +13,3 @@ pub mod theme_toggle;
 //
 pub mod demo_quick_test;
 pub mod demo_template;
-
-pub mod demo_tabs_shadow_2;
-
-//
-pub mod cards_glowing;
-pub mod demo_card3d_flip_rotation;
-pub mod demo_cards_repushing;
-pub mod demo_parallax1;
-pub mod demo_scroll_to_top_button;
--- LABS-INTERNAL/src/components/nooooooooooo/mod.rs
+++ LABS-INTERNAL/src/components/nooooooooooo/mod.rs
@@ -1,5 +1,6 @@
 //
 pub mod demo_cards_follow_shadow;
+pub mod demo_cards_repushing;
 pub mod demo_cards_stacking;
 pub mod demo_carousel_snap_scroll;
 pub mod demo_cursor_gooey_fluid;
@@ -8,3 +9,4 @@ pub mod demo_otp;
 pub mod demo_scroll_horizontal_awaards;
 pub mod demo_scroll_style_queries;
 pub mod demo_scrollspy;
+pub mod demo_tabs_shadow_2;
